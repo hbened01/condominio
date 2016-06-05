e6a7dcd35dc19af43dd4f2a67e8f58a81a9e4e7c
humberto benedito <humbertobenedito@gmail.com>
2016-06-05 17:53:23 -0430

    Se eliminó yii2-admin y se adecuó la plantilla lte-admin.

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -12,9 +12,10 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
+        //'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css',
+        //'https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css',
         'css/bootstrap.min.css',
-        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css',
-        'https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css',
+        'css/font-awesome.css',
         'css/AdminLTE.min.css',
         'css/skins/_all-skins.min.css',
         'css/flat/blue.css',
@@ -26,16 +27,19 @@ class AppAsset extends AssetBundle
         'css/site.css',
     ];
     public $js = [
+        //'https://code.jquery.com/ui/1.11.4/jquery-ui.min.js',
+        //'https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js',
+        //'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js',
         'js/jQuery-2.2.0.min.js',
-        'https://code.jquery.com/ui/1.11.4/jquery-ui.min.js',
+        'js/jquery-ui.min.js'
         'js/bootstrap.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js',
+        'js/raphael-min.js'
         'js/morris.min.js',
         'js/jquery.sparkline.min.js',
         'js/jquery-jvectormap-1.2.2.min.js',
         'js/jquery-jvectormap-world-mill-en.js',
         'js/jquery.knob.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js',
+        'js/moment.min.js'
         'js/daterangepicker.js',
         'js/bootstrap-datepicker.js',
         'js/bootstrap3-wysihtml5.all.min.js',
