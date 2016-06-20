dfc18f6c69ea18c86a2bf4ba249d05747447fdcf
hbened01 <humbertobenedito@gmail.com>
2016-06-20 12:01:58 -0400

    actualización de logo

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -64,7 +64,7 @@ $baseUrl = $asset->baseUrl;
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="index.html"><img src="<?=$baseUrl?>/dist/img/logo.png" alt="logo"></a>
+                    <a class="navbar-brand" href="index.html"><img src="<?=$baseUrl?>/dist/img/logo_condominio.png" alt="logo"></a>
                 </div>
                 
                 <div class="collapse navbar-collapse navbar-right">
