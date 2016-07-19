a3c287b261b8c55ff76fb2fab076748f8b0499b2
javc_8578 <joahdner.vazquez@hotmail.com>
2016-07-18 22:35:14 -0400

    Nueva modificación del shell script

--- bk_database/carga_facturas.sh
+++ bk_database/carga_facturas.sh
@@ -1,76 +1,74 @@
-#! /bin/bash
-#path=/home/jvazqu03/
+#!/bin/bash
+#path=/home/hbened01/
 path=/home/johander/
 file="copiacondc.csv"
 
-#sed -i '\n' $path$file
 awk '/"CONJ. RES."/' $path$file > $path"copiacondc.txt"
-sed 's/\"//g' $path"copiacondc.txt" > $path"copiacondc2.txt"
+sed 's/",*/;/g' $path"copiacondc.txt" > $path"copiacondc2.txt"
+sed 's/,;/;/g' $path"copiacondc2.txt" > $path"copiacondc3.txt"
 
 tempApto=""
 tempResi=""
 tempGastos=""
+tempFondos=""
+
 while read line
 do
-	#$campo = `cut -d "," -f 1 $path$file`
 
-	resi=$(echo  "$line" | cut -f2 -d',')
+	resi=$(echo  "$line" | cut -f28 -d';')
 	if [ "$tempResi" != "$resi" ]
 		then
 		tempResi=$resi
 	fi
 
-	apto=$(echo  "$line" | cut -f14 -d',')
+	apto=$(echo  "$line" | cut -f26 -d';')
 	if [ "$tempApto" != "$apto" ]
 		then
 		tempApto=$apto
 		tempGastos=""
-		apellido=$(echo  "$line" | cut -f16 -d',')
-		nombre=$(echo  "$line" | cut -f17 -d',')
-		mes=$(echo  "$line" | cut -f19 -d',')
-		anio=$(echo  "$line" | cut -f20 -d',')
+		propietario=$(echo  "$line" | cut -f30 -d';')
+		mes=$(echo  "$line" | cut -f32 -d';')
+		anio=$(echo  "$line" | cut -f33 -d';')
 		echo "***********************************************************************"
-		echo $resi - $apto - $nombre $apellido - $mes $anio
+		echo $resi - $apto - $propietario - $mes $anio
 		echo "-----------------------------------------------------------------------"
 
 		if [ "$tempApto" = "405" ]
 			then
 			echo $line
 		fi
+
+		fondo=$(echo  "$line" | cut -f56 -d';')
+		if  [ "$fondo" = "Fondos" ]
+			then
+			echo ""
+			echo "--------- $fondo --------"
+			echo ""
+
+			i=66
+			j=0
+			while [ "$(echo  "$line" | cut -f$i -d';')" != "" ]
+			do
+				#arr[$j]=$(echo  "$line" | cut -f$i -d';')
+				echo $(echo  "$line" | cut -f$i -d';')
+				i=`expr $i + 2`
+				j=`expr $j + 1`
+			done
+		fi
 	fi
 
-	gastos=$(echo  "$line" | cut -f23 -d',')
-	cpto=$(echo  "$line" | cut -f24 -d',')
+	gastos=$(echo  "$line" | cut -f38 -d';')
 
 	if [ "$tempGastos" != "$gastos" ]
 		then
 		tempGastos=$gastos
-		if [ "$tempGastos" = "Monto" ]
-			then
-			echo ""
-			echo "--------- $cpto --------"
-			echo ""
-		else
-			echo ""
-			echo "--------- $gastos --------"
-			echo ""
-		fi
+		echo ""
+		echo "--------- $gastos --------"
+		echo ""
 	fi
 
-	if [ "$cpto" = "Gastos Comunes" ]
-		then
-		cpto=$(echo  "$line" | cut -f25 -d',')
-		totalcpto=$(echo  "$line" | cut -f26 -d',')
-	else
-		totalcpto=$(echo  "$line" | cut -f25 -d',')
-	fi
+	cpto=$(echo  "$line" | cut -f40 -d';')
+	totalcpto=$(echo  "$line" | cut -f41 -d';')
 	echo $cpto - $totalcpto
 
-	fondo=$(echo  "$line" | cut -f40 -d',')
-	if [ "$tempApto" != "$apto" ] && [ "$fondo" = "Fondos" ]
-		then
-		echo ""
-		echo "--------- $fondo --------"
-		echo ""
-	fi
-done < $path"copiacondc2.txt"
+done < $path"copiacondc3.txt"
