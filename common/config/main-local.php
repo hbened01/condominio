9139a1a61de457608176c6c93cae101755ec6383
Vazquez Castillo Johander <jvazqu03@cantv.com.ve>
2016-08-03 11:53:05 -0400

    ss

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -8,27 +8,27 @@ return [
             'password' => '',
             'charset' => 'utf8',
         ],*/
-        // 'db' => [
-        //     'class' => 'yii\db\Connection',
-        //     'dsn' => 'pgsql:host=161.196.12.244;dbname=condominio_db',
-        //     'username' => 'postgres',
-        //     'password' => '<zaq12wsx',
-        //     'charset' => 'utf8',
-        // ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'pgsql:host=161.196.12.244;dbname=condominio_db',
+            'username' => 'postgres',
+            'password' => '<zaq12wsx',
+            'charset' => 'utf8',
+        ],
         // 'db' => [
         //     'class' => 'yii\db\Connection',
         //     'dsn' => 'pgsql:host=localhost;dbname=condominio_db',
         //     'username' => 'postgres',
         //     'password' => '<zaq12wsx',
         //     'charset' => 'utf8',
+        // // ],
+        // 'db' => [
+        //     'class' => 'yii\db\Connection',
+        //     'dsn' => 'pgsql:host=127.0.0.1;dbname=hjtecnos_condominio',
+        //     'username' => 'hjtecnos',
+        //     'password' => '1ytZq26fH1',
+        //     'charset' => 'utf8',
         // ],
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'pgsql:host=127.0.0.1;dbname=hjtecnos_condominio',
-            'username' => 'hjtecnos',
-            'password' => '1ytZq26fH1',
-            'charset' => 'utf8',
-        ],
        /* 'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
